6206dc7cbdd64b3bf120897e85311bf59d1236d5
BintangKus <bintangkus308@gmail.com>
2024-03-22 08:16:20 +0700

    Langkah Ke-13 - Membungkus tombol pada Home Page dengan Column dan tambahkan tombol baru.

--- praktikum_05/lib/second_page.dart
+++ praktikum_05/lib/second_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
-
 class SecondPage extends StatelessWidget {
   final String? data;
 
   const SecondPage({Key? key, this.data}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final data2 = ModalRoute.of(context)!.settings.arguments ?? '';
     return Scaffold(
       appBar: AppBar(
         title: const Text('Second Page'),
@@ -17,6 +18,10 @@ class SecondPage extends StatelessWidget {
           children: [
             Text(
               data ?? '',
+              style: const TextStyle(fontSize: 20.0),
+            ),
+            Text(
+              data2.toString(),
               style: const TextStyle(fontSize: 20.0),
             ),
             ElevatedButton(
