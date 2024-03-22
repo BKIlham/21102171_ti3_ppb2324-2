f9d5e2d4ec2444a3201e0371668f19995273d3d2
BintangKus <bintangkus308@gmail.com>
2024-03-22 07:47:44 +0700

    Langkah Ke-10 - Menambahkan variable data pada SecondPage dan mengubah constructor SecondPage

--- praktikum_05/lib/second_page.dart
+++ praktikum_05/lib/second_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
-  const SecondPage({Key? key}) : super(key: key);
+  final String? data;
+
+  const SecondPage({Key? key, this.data}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
