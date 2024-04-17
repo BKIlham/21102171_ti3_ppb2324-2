5ee11cb78bb45c8589ccc2eed2d9945c46ece584
BintangKus <bintangkus308@gmail.com>
2024-04-17 17:26:35 +0700

    50. Portfolio Page Header

--- saving_app (Pertemuan 6-7)/saving_app/lib/pages/portofolio/portofolio_page.dart
+++ saving_app (Pertemuan 6-7)/saving_app/lib/pages/portofolio/portofolio_page.dart
@@ -1,13 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:saving_app/styles/colors.dart';
+import 'package:saving_app/styles/text_styles.dart';
 
 class PortofolioPage extends StatelessWidget {
   const PortofolioPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Text('Portofolio'),
-    ));
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 25),
+            height: 240,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(40)),
+              image: DecorationImage(
+                image: AssetImage('assets/images/bg-container-2.png'),
+                fit: BoxFit.cover,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: kGrey,
+                  blurRadius: 5,
+                  offset: Offset.fromDirection(2),
+                ),
+              ],
+            ),
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Text(
+                    'My Portofolio',
+                    style: kHeading6.copyWith(
+                        color: kWhite, fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Saving Value',
+                    style: kSubtitle2.copyWith(
+                      color: kWhite,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Rp 12.480.000',
+                    style: kHeading5.copyWith(
+                      color: kWhite,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
