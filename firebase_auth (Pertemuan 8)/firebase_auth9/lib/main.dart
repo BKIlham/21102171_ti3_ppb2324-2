e925698582e0e61d6caa8b37b388849eb68c393e
BintangKus <bintangkus308@gmail.com>
2024-05-03 09:30:25 +0700

    login

--- firebase_auth (Pertemuan 8)/firebase_auth9/lib/main.dart
+++ firebase_auth (Pertemuan 8)/firebase_auth9/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth9/bloc/login/login_cubit.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth9/ui/splash.dart';
 import 'package:firebase_auth9/utils/routes.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -17,12 +19,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Praktikum 6",
-      debugShowCheckedModeBanner: false,
-      navigatorKey: NAV_KEY,
-      onGenerateRoute: generateRoute,
-      home: SplashScreen(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => LoginCubit())],
+      child: MaterialApp(
+        title: "Praktikum 6",
+        debugShowCheckedModeBanner: false,
+        navigatorKey: NAV_KEY,
+        onGenerateRoute: generateRoute,
+        home: SplashScreen(),
+      ),
     );
   }
 }
