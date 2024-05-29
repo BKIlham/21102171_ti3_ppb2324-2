970fc083073f66b9080d69729f58265ddfb6e0ad
BintangKus <bintangkus308@gmail.com>
2024-05-29 19:45:22 +0700

    Bintang ganteng membuat halaman login

--- firebase_auth (Pertemuan 8)/firebase_auth9/test/widget_test.dart
+++ firebase_auth (Pertemuan 8)/firebase_auth9/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:firebase_auth9/main.dart';
+import '../../../../Tugas/Sahabat/sahabat_air/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
